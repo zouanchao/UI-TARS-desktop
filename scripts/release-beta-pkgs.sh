f26d89a6b4a4b0439371f5eaa2a0d9e1040246d6
jinxin001 <jinxin001@bytedance.com>
2025-02-16 23:55:47 +0800

    chore: remove pre pack

--- scripts/release-beta-pkgs.sh
+++ scripts/release-beta-pkgs.sh
@@ -12,18 +12,18 @@ pnpm changeset version
 echo "2. update dependencies..."
 pnpm install
 
-echo "3. commit version update..."
-git add .
-git commit -m "release: publish beta packages"
-
-echo "4. create release tag..."
+echo "3. create release tag..."
 pnpm changeset tag
 
-echo "5. publish to npm..."
+echo "4. publish to npm..."
 pnpm publish -r --no-git-checks --access public --tag beta
 
-echo "6. exit changeset..."
+echo "5. exit changeset..."
 pnpm changeset pre exit
+
+echo "6. commit version update..."
+git add .
+git commit -m "release: publish beta packages"
 
 echo "7. prepare to push to remote git repository..."
 read -p "confirm push to remote git repository? (y/N) " confirm
