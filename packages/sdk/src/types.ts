358b9652cb5cdfa35672ea6758aa8c58ba12b841
jinxin001 <jinxin001@bytedance.com>
2025-02-16 23:28:55 +0800

    feat: sdk ready

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -2,3 +2,63 @@
  * Copyright (c) 2025 Bytedance, Inc. and its affiliates.
  * SPDX-License-Identifier: Apache-2.0
  */
+import {
+  Message,
+  GUIAgentData,
+  PredictionParsed,
+  ScreenshotResult,
+} from '@ui-tars/shared/types';
+
+import { BaseOperator, BaseModel } from './base';
+import { UITarsModel } from './Model';
+export interface ExecuteParams {
+  prediction: PredictionParsed;
+  screenWidth: number;
+  screenHeight: number;
+}
+
+export interface ScreenshotOutput extends ScreenshotResult {}
+
+export interface InvokeParams {
+  conversations: Message[];
+  images: string[];
+}
+
+export interface InvokeOutput {
+  prediction: string;
+  parsedPredictions: PredictionParsed[];
+}
+export abstract class Operator<T = unknown> extends BaseOperator<T> {
+  constructor(config?: T) {
+    super(config);
+  }
+  abstract screenshot(): Promise<ScreenshotOutput>;
+  abstract execute(params: ExecuteParams): Promise<void>;
+}
+
+export abstract class Model<T = unknown> extends BaseModel<T> {
+  abstract get factor(): number;
+  abstract invoke(params: InvokeParams): Promise<InvokeOutput>;
+}
+
+export type Logger = Pick<Console, 'log' | 'error' | 'warn' | 'info'>;
+
+export interface GUIAgentConfig<TOperator> {
+  operator: TOperator;
+  model: ConstructorParameters<typeof UITarsModel>[0];
+
+  // ===== Optional =====
+  systemPrompt?: string;
+  signal?: AbortSignal;
+  onData?: (params: { data: GUIAgentData }) => void;
+  onError?: (params: {
+    data: GUIAgentData;
+    error: {
+      // TODO: define error code
+      code: number;
+      error: string;
+      stack?: string;
+    };
+  }) => void;
+  logger?: Logger;
+}
