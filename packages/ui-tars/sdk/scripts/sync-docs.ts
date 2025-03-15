c02ecfe20180008f53dae6dc487509b18b293423
jinxin001 <jinxin001@bytedance.com>
2025-03-15 18:19:55 +0800

    refactor: monorepo

--- packages/ui-tars/sdk/scripts/sync-docs.ts
+++ packages/ui-tars/sdk/scripts/sync-docs.ts
@@ -6,7 +6,7 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 
 const packageDirPath = path.join(__dirname, '..');
-const docFilePath = path.join(packageDirPath, '../../docs/sdk.md');
+const docFilePath = path.join(packageDirPath, '../../../docs/sdk.md');
 
 async function syncDocs() {
   try {
